fa643100b51fb83b2a133f2560a387de66e5fccd
arthur <arthur.teisseire@epitech.eu>
2019-04-10 17:56:03 +0200

    Add Vector data type

--- package/app/Main.hs
+++ package/app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
-import Lib
+import           Lib
+
+data Vector = Vector
+    { x :: Int
+    , y :: Int
+    , z :: Int
+    } deriving (Show)
+
+vplus :: Vector -> Vector -> Vector
+(Vector i j k) `vplus` (Vector l m n) = Vector (i + m) (j + m) (k + n)
 
 main :: IO ()
-main = someFunc
+main = print $ show $ Vector 1 2 3 `vplus` Vector 4 5 6
