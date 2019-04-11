0bc86877e2165268bb8be189fac0e240b5c25415
arthur <arthur.teisseire@epitech.eu>
2019-04-11 23:39:36 +0200

    Add a check for nb args using IO monad

--- package/app/Main.hs
+++ package/app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import System.Environment
+import System.Exit
 import Vector
 
 main :: IO ()
-main = print $ show $ Vector 1 2 3 `vplus` Vector 4 5 6
+main = getArgs >>= checkNbArgs >>= rFile
+
+checkNbArgs :: [String] -> IO [String]
+checkNbArgs args
+    | length args == 3 = return args
+    | otherwise = exitWith (ExitFailure 84)
+
+rFile :: [String] -> IO ()
+rFile (fileName:args) = putStrLn fileName
--- package/src/Vector.hs
+++ package/src/Vector.hs
@@ -10,4 +10,4 @@ vplus :: Vector -> Vector -> Vector
 (Vector x1 y1 z1) `vplus` (Vector x2 y2 z2) = Vector (x1 + x2) (y1 + y2) (z1 + z2)
 
 vdist :: Vector -> Vector -> Float
-(Vector x1 y1 z1) `vdist` (Vector x2 y2 z2) = sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2 + (z1 - z2) ^ 2)
+(Vector x1 y1 z1) `vdist` (Vector x2 y2 z2) = sqrt $ (x1 - x2) ^ 2 + (y1 - y2) ^ 2 + (z1 - z2) ^ 2
