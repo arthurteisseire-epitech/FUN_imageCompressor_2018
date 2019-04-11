777beee4fbfba65b3b3d62b9ba3599ca25e99a8d
arthur <arthur.teisseire@epitech.eu>
2019-04-11 11:28:23 +0200

    Add vdist to calc euclidian distance between two Vector

--- package/src/Vector.hs
+++ package/src/Vector.hs
@@ -1,11 +1,13 @@
 module Vector where
 
 data Vector = Vector
-    { x :: Int
-    , y :: Int
-    , z :: Int
+    { x :: Float
+    , y :: Float
+    , z :: Float
     } deriving (Show, Eq)
 
 vplus :: Vector -> Vector -> Vector
-(Vector i j k) `vplus` (Vector l m n) = Vector (i + l) (j + m) (k + n)
+(Vector x1 y1 z1) `vplus` (Vector x2 y2 z2) = Vector (x1 + x2) (y1 + y2) (z1 + z2)
 
+vdist :: Vector -> Vector -> Float
+(Vector x1 y1 z1) `vdist` (Vector x2 y2 z2) = sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2 + (z1 - z2) ^ 2)
--- package/test/Spec.hs
+++ package/test/Spec.hs
@@ -12,5 +12,8 @@ vectorEquals = TestCase $ assertEqual "Vectors Equals" (Vector 1 2 3) (Vector 1 
 vectorPlus :: Test
 vectorPlus = TestCase $ assertEqual "Vector Plus" (Vector 2 4 6) (Vector 1 2 3 `vplus` Vector 1 2 3)
 
+vectorDist :: Test
+vectorDist = TestCase $ assertEqual "Vector Dist" 5 (Vector 3 4 5 `vdist` Vector 3 8 2)
+
 tests :: Test
-tests = TestList [vectorEquals, vectorPlus]
+tests = TestList [vectorEquals, vectorPlus, vectorDist]
