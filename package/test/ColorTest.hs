fc24d9180b4cec4034c998468697b24be8320db6
arthur <arthur.teisseire@epitech.eu>
2019-04-12 15:11:01 +0200

    Add ColorTest file and include it in Spec.hs

---
+++ package/test/ColorTest.hs
@@ -0,0 +1,16 @@
+module ColorTest where
+
+import           Test.HUnit
+import           Color
+
+colorEqual :: Test
+colorEqual = TestCase $ assertEqual "Color Equals" (Color 1 2 3) (Color 1 2 3)
+
+colorPlus :: Test
+colorPlus = TestCase $ assertEqual "Color Plus" (Color 2 4 6) (Color 1 2 3 `vplus` Color 1 2 3)
+
+colorDist :: Test
+colorDist = TestCase $ assertEqual "Color Dist" 5 (Color 3 4 5 `vdist` Color 3 8 2)
+
+colorTests :: Test
+colorTests = TestList [colorEqual, colorPlus, colorDist]
--- package/test/Spec.hs
+++ package/test/Spec.hs
@@ -1,19 +1,10 @@
 import           Test.HUnit
-import           Color
+import ColorTest
 
 main :: IO ()
 main = do
     runTestTT tests
     return ()
 
-vectorEquals :: Test
-vectorEquals = TestCase $ assertEqual "Color Equals" (Color 1 2 3) (Color 1 2 3)
-
-vectorPlus :: Test
-vectorPlus = TestCase $ assertEqual "Color Plus" (Color 2 4 6) (Color 1 2 3 `vplus` Color 1 2 3)
-
-vectorDist :: Test
-vectorDist = TestCase $ assertEqual "Color Dist" 5 (Color 3 4 5 `vdist` Color 3 8 2)
-
 tests :: Test
-tests = TestList [vectorEquals, vectorPlus, vectorDist]
+tests = colorTests
