57d958660e6da5659a2524963007b0997b909d6a
arthur <arthur.teisseire@epitech.eu>
2019-04-13 11:28:45 +0200

    Add pointParser

--- package/src/Point.hs
+++ package/src/Point.hs
@@ -1,6 +1,24 @@
 module Point where
 
+import Text.ParserCombinators.ReadP
+import Data.Char
+
 data Point = Point
     { x :: Float
     , y :: Float
     } deriving (Show, Eq)
+
+digit :: ReadP Char
+digit = satisfy isDigit
+
+pointParser :: ReadP Point
+pointParser = do
+    satisfy (== '(')
+    x <- read <$> many1 digit
+    satisfy (== ',')
+    y <- read <$> many1 digit
+    satisfy (== ')')
+    return (Point x y)
+
+pointParse :: String -> Point
+pointParse s = fst $ head $ readP_to_S pointParser s
--- package/test/PointTest.hs
+++ package/test/PointTest.hs
@@ -6,5 +6,11 @@ import           Test.HUnit
 pointEqual :: Test
 pointEqual = TestCase $ assertEqual "Point Equal" (Point 1 2) (Point 1 2)
 
+pointParserTest :: Test
+pointParserTest = TestCase $ assertEqual "Point parser" (pointParse "(1,2)") (Point 1 2)
+
+pointParserMultipleDigitTest :: Test
+pointParserMultipleDigitTest = TestCase $ assertEqual "Point parser" (pointParse "(11,22)") (Point 11 22)
+
 pointTests :: [Test]
-pointTests = [pointEqual]
+pointTests = [pointEqual, pointParserTest, pointParserMultipleDigitTest]
