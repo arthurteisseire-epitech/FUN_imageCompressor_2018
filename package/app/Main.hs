fd2b7a0e388878bebf5c9df9b9426b38237c077e
arthur <arthur.teisseire@epitech.eu>
2019-04-13 14:59:47 +0200

    Create Pixel array from file

--- package/app/Main.hs
+++ package/app/Main.hs
@@ -2,15 +2,28 @@ module Main where
 
 import System.Environment
 import System.Exit
-import Color
+import Pixel
+import Data.Maybe
 
 main :: IO ()
-main = getArgs >>= checkNbArgs >>= readInputFile
+main = getArgs >>= checkNbArgs >>= getPixels >>= compute
 
 checkNbArgs :: [String] -> IO [String]
 checkNbArgs args
     | length args == 3 = return args
-    | otherwise = exitWith (ExitFailure 84)
+    | otherwise = exitWithHelp
 
-readInputFile :: [String] -> IO ()
-readInputFile (fileName:args) = putStrLn fileName
+compute :: [Maybe Pixel] -> IO ()
+compute args = putStrLn "toto"
+
+getPixels :: [String] -> IO [Maybe Pixel]
+getPixels args = fileToPixels (head args)
+
+fileToPixels :: String -> IO [Maybe Pixel]
+fileToPixels fileName = readFile fileName >>= textToPixels
+
+textToPixels :: String -> IO [Maybe Pixel]
+textToPixels text = return $ map strToPixel (lines text)
+
+exitWithHelp :: IO a
+exitWithHelp = exitWith (ExitFailure 84)
