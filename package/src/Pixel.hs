0f57b9886895abac28451999b9292a55f137aceb
arthur <arthur.teisseire@epitech.eu>
2019-04-13 12:42:15 +0200

    Add pixelParser

--- package/src/Pixel.hs
+++ package/src/Pixel.hs
@@ -1,5 +1,7 @@
 module Pixel where
 
+import Text.ParserCombinators.ReadP
+import Data.Char
 import Point
 import Color
 
@@ -7,3 +9,15 @@ data Pixel = Pixel
     { point :: Point
     , color :: Color
     } deriving (Show, Eq)
+
+pixelParser :: ReadP Pixel
+pixelParser = do
+    point <- pointParser
+    satisfy (== ' ')
+    color <- colorParser
+    return (Pixel point color)
+
+strToPixel :: String -> Maybe Pixel
+strToPixel s
+    | not $ null $ readP_to_S pixelParser s = Just $ fst $ head $ readP_to_S pixelParser s
+    | otherwise = Nothing
--- package/test/PixelTest.hs
+++ package/test/PixelTest.hs
@@ -8,5 +8,13 @@ import           Test.HUnit
 pixelEqual :: Test
 pixelEqual = TestCase $ assertEqual "Pixel Equal" (Pixel (Point 1 2) (Color 1 2 3)) (Pixel (Point 1 2) (Color 1 2 3))
 
+pixelParserTest :: Test
+pixelParserTest = TestCase $ assertEqual "Point parser" (Just $ Pixel (Point 1 2) (Color 1 2 3)) (strToPixel "(1,2) (1,2,3)")
+
+pixelParserTestFail :: Test
+pixelParserTestFail = TestCase $ assertEqual "Point parser" Nothing (strToPixel "(1,2)(1,2,3)")
+
 pixelTests :: [Test]
-pixelTests = [pixelEqual]
+pixelTests = [ pixelEqual
+             , pixelParserTest
+             ]
