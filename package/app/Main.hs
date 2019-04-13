8b89d04b6cb4ca0549b05e15f03bcb6366bfa88a
arthur <arthur.teisseire@epitech.eu>
2019-04-13 23:00:05 +0200

    Add getRandomPixel function

--- package/app/Main.hs
+++ package/app/Main.hs
@@ -10,8 +10,13 @@ main :: IO ()
 main = getArgs >>= parseArgs >>= getPixels >>= compute
 
 compute :: [Pixel] -> IO ()
-compute []     = putStrLn "No valid pixels given"
-compute pixels = printPixel $ head pixels
+compute []     = putStrLn "No valid pixel given"
+compute pixels = getRandomPixel pixels >>= printPixel
+
+getRandomPixel :: [Pixel] -> IO Pixel
+getRandomPixel pixels = do
+    gen <- getStdGen
+    return $ pixels !! fst (randomR (0, length pixels - 1) gen :: (Int, StdGen))
 
 getPixels :: (Int, Float, String) -> IO [Pixel]
 getPixels args = catMaybes <$> fileToPixels (third args)
