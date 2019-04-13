108bcf42f47aba87814f542f6fae7b5485b5ccf8
arthur <arthur.teisseire@epitech.eu>
2019-04-13 18:37:41 +0200

    Add findClosestColor

--- package/src/Color.hs
+++ package/src/Color.hs
@@ -1,7 +1,14 @@
-module Color where
+module Color
+    ( findClosestColor
+    , vdist
+    , vplus
+    , colorToStr
+    , colorParser
+    , strToColor
+    ) where
 
-import Text.ParserCombinators.ReadP
-import Data.Char
+import           Data.Char
+import           Text.ParserCombinators.ReadP
 
 data Color = Color
     { r :: Int
@@ -15,6 +22,16 @@ vplus :: Color -> Color -> Color
 vdist :: Color -> Color -> Float
 (Color r1 g1 b1) `vdist` (Color r2 g2 b2) = sqrt $ fromIntegral $ (r1 - r2) ^ 2 + (g1 - g2) ^ 2 + (b1 - b2) ^ 2
 
+findClosestColor :: [Color] -> Color -> Color
+findClosestColor [] _         = Color 0 0 0
+findClosestColor colors color = findClosestColor' colors color (head colors)
+
+findClosestColor' :: [Color] -> Color -> Color -> Color
+findClosestColor' [] _ minColor = minColor
+findClosestColor' (x:xs) color minColor
+    | (minColor `vdist` color) < (x `vdist` color) = findClosestColor' xs color minColor
+    | otherwise = findClosestColor' xs color x
+
 isWord8 :: Int -> Bool
 isWord8 n = n >= 0 && n < 256
 
@@ -27,10 +44,9 @@ colorParser = do
     satisfy (== ',')
     z <- read <$> many1 (satisfy isDigit)
     satisfy (== ')')
-    if isWord8 x && isWord8 y && isWord8 z then
-        return (Color x y z)
-    else
-        pfail
+    if isWord8 x && isWord8 y && isWord8 z
+        then return (Color x y z)
+        else pfail
 
 strToColor :: String -> Maybe Color
 strToColor s
--- package/test/ColorTest.hs
+++ package/test/ColorTest.hs
@@ -12,17 +12,26 @@ colorPlus = TestCase $ assertEqual "Color Plus" (Color 2 4 6) (Color 1 2 3 `vplu
 colorDist :: Test
 colorDist = TestCase $ assertEqual "Color Dist" 5 (Color 3 4 5 `vdist` Color 3 8 2)
 
+findClosestColorTest :: Test
+findClosestColorTest = TestCase $ assertEqual "Color Find Closest" (Color 35 18 111) (findClosestColor
+                       [ Color 33 18 109
+                       , Color 33 17 109
+                       , Color 35 18 111
+                       , Color 35 21 109
+                       , Color 38 21 112
+                       ] (Color 34 18 112))
+
 colorParserTest :: Test
-colorParserTest = TestCase $ assertEqual "Point parser" (Just $ Color 1 2 3) (strToColor "(1,2,3)")
+colorParserTest = TestCase $ assertEqual "Color parser" (Just $ Color 1 2 3) (strToColor "(1,2,3)")
 
 colorParserTestTooBig :: Test
-colorParserTestTooBig = TestCase $ assertEqual "Point parser" Nothing (strToColor "(256,2,3)")
+colorParserTestTooBig = TestCase $ assertEqual "Color parser" Nothing (strToColor "(256,2,3)")
 
 colorParserTestMultipleDigit :: Test
-colorParserTestMultipleDigit = TestCase $ assertEqual "Point parser" (Just $ Color 123 234 123) (strToColor "(123,234,123)")
+colorParserTestMultipleDigit = TestCase $ assertEqual "Color parser" (Just $ Color 123 234 123) (strToColor "(123,234,123)")
 
 colorParserTestNegative :: Test
-colorParserTestNegative = TestCase $ assertEqual "Point parser" Nothing (strToColor "(-1,1,1)")
+colorParserTestNegative = TestCase $ assertEqual "Color parser" Nothing (strToColor "(-1,1,1)")
 
 colorTests :: [Test]
 colorTests = [ colorEqual
@@ -32,4 +41,5 @@ colorTests = [ colorEqual
              , colorParserTestTooBig
              , colorParserTestMultipleDigit
              , colorParserTestNegative
+             , findClosestColorTest
              ]
